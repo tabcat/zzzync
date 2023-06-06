09513d3fdc156598df1544b333ebf633f2faee63
tabcat <tabcat00@proton.me>
2023-06-06 14:51:06 -0500

    chore: .aegir.js doesnt rely on build

--- .aegir.js
+++ .aegir.js
@@ -1,5 +1,71 @@
 import getPort from 'aegir/get-port'
-import { createHeliaNode } from './dist/test/utils/create-helia.js'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { tcp } from '@libp2p/tcp'
+import { webSockets } from '@libp2p/websockets'
+import * as filters from '@libp2p/websockets/filters'
+import { MemoryDatastore } from 'datastore-core'
+import { createLibp2p, } from 'libp2p'
+import { circuitRelayServer, } from 'libp2p/circuit-relay'
+import { kadDHT } from '@libp2p/kad-dht'
+import { ipnsSelector } from 'ipns/selector'
+import { ipnsValidator } from 'ipns/validator'
+import { identifyService } from 'libp2p/identify'
+import { MemoryBlockstore } from 'blockstore-core'
+import { createHelia } from 'helia'
+
+const services = {
+  identify: identifyService(),
+  dht: kadDHT({
+    validators: { ipns: ipnsValidator },
+    selectors: { ipns: ipnsSelector }
+  })
+}
+
+async function createLibp2pNode (options) {
+  const datastore = new MemoryDatastore()
+  return createLibp2p({
+    addresses: {
+      listen: [
+        '/ip4/127.0.0.1/tcp/0/ws'
+      ]
+    },
+    transports: [
+      tcp(),
+      webSockets({
+        filter: filters.all
+      })
+    ],
+    connectionEncryption: [
+      noise()
+    ],
+    streamMuxers: [
+      yamux()
+    ],
+    datastore,
+    ...options,
+    services: {
+      ...services,
+      relay: circuitRelayServer({ advertise: true })
+    }
+  })
+}
+
+export async function createHeliaNode (init) {
+  const blockstore = new MemoryBlockstore()
+  const datastore = new MemoryDatastore()
+
+  const libp2p = await createLibp2pNode()
+
+  const helia = await createHelia({
+    libp2p,
+    blockstore,
+    datastore,
+    ...init
+  })
+
+  return helia
+}
 
 /** @type {import('aegir').PartialOptions} */
 export default {
