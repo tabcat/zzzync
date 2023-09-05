1d23cffdb8a05b41d7afd7e6da45b832596efa7a
tabcat <tabcat00@proton.me>
2023-09-05 14:55:58 +0100

    feat!: add pinner to zzzync interface

--- src/index.ts
+++ src/index.ts
@@ -9,41 +9,41 @@
 
 import type { Ed25519PeerId } from '@libp2p/interface/peer-id'
 import type { QueryEvent } from '@libp2p/kad-dht'
+import type { Blockstore } from 'interface-blockstore'
 import type { CID } from 'multiformats/cid'
 
 export { toDcid } from './dcid.js'
-
-export interface Namer {
-  publish: (key: Ed25519PeerId, value: CID) => Promise<void>
-  resolve: (key: Ed25519PeerId) => Promise<CID>
-}
 
 export interface Advertiser {
   collaborate: (dcid: CID, provider: Ed25519PeerId) => AsyncIterable<QueryEvent>
   findCollaborators: (dcid: CID) => AsyncIterable<QueryEvent>
 }
 
+export interface Namer {
+  publish: (key: Ed25519PeerId, value: CID) => Promise<void>
+  resolve: (key: Ed25519PeerId) => Promise<CID>
+}
+
+export interface Pinner extends Blockstore {}
+
 export interface Zzzync {
-  namer: Namer
-  advertiser: Advertiser
+  readonly namer: Namer
+  readonly advertiser: Advertiser
+  readonly pinner: Pinner
 }
 
 class DefaultZzzync implements Zzzync {
-  readonly namer: Namer
-  readonly advertiser: Advertiser
-
   constructor (
-    namer: Namer,
-    advertiser: Advertiser
-  ) {
-    this.namer = namer
-    this.advertiser = advertiser
-  }
+    readonly namer: Namer,
+    readonly advertiser: Advertiser,
+    readonly pinner: Pinner
+  ) {}
 }
 
 export function zzzync (
   namer: Namer,
-  advertiser: Advertiser
+  advertiser: Advertiser,
+  pinner: Pinner
 ): Zzzync {
-  return new DefaultZzzync(namer, advertiser)
+  return new DefaultZzzync(namer, advertiser, pinner)
 }
