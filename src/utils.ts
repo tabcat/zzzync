9d8bf1cd6017b3eadfe309a4845447b0968271a3
tabcat <tabcat00@proton.me>
2026-01-09 13:23:11 -0600

    cleaner errors

--- src/utils.ts
+++ src/utils.ts
@@ -5,13 +5,13 @@ export function parseRecordValue(
 	value: string,
 ): CID<unknown, typeof CODEC_DAG_PB, number, 1> {
 	if (!value.startsWith(IPFS_PREFIX)) {
-		throw new Error("RECORD_VALUE_UNSUPPORTED");
+		throw new TypeError('value is missing /ipfs/ prefix');
 	}
 
 	const cid = CID.parse(value.substring(IPFS_PREFIX.length));
 
 	if (cid.code !== CODEC_DAG_PB) {
-		throw new Error("ERR_EXPECTED_UNIXFS");
+		throw new TypeError('value cid code is not dag-pb');
 	}
 
 	return cid as CID<unknown, typeof CODEC_DAG_PB, number, 1>;
