6572fcd33c44625e87159a745764e521becab78b
tabcat <tabcat00@proton.me>
2026-01-20 20:02:13 +0530

    no top level await

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -27,55 +27,56 @@ const help: SubCommand = {
 	run: (_args: string[]) => console.log(HELP_MESSAGE),
 };
 
-let subcommand: SubCommand | null = null;
-switch (cmd) {
-	case "daemon": {
-		subcommand = (await import(join(__dirname, "daemon.js"))) as SubCommand;
-		break;
+(async () => {
+	let subcommand: SubCommand | null = null;
+	switch (cmd) {
+		case "daemon": {
+			subcommand = (await import(join(__dirname, "daemon.js"))) as SubCommand;
+			break;
+		}
+		case "push": {
+			subcommand = (await import(join(__dirname, "push.js"))) as SubCommand;
+			break;
+		}
+		case "help": {
+			subcommand = help;
+			break;
+		}
 	}
-	case "push": {
-		subcommand = (await import(join(__dirname, "push.js"))) as SubCommand;
-		break;
+
+	if (subcommand == null) {
+		throw new Error(`Unrecognized command: ${cmd}`);
 	}
-	case "help": {
-		subcommand = help;
-		break;
+
+	let stopping: Promise<void> | false = false;
+	function stop() {
+		if (!stopping)
+			stopping = (async () => {
+				try {
+					await subcommand?.cleanup?.();
+				} catch (e) {
+					console.error("Failed while running cleanup");
+					console.error(e);
+					process.exitCode = 1;
+				}
+			})();
+		return stopping;
 	}
-}
+	process.once("SIGINT", async () => {
+		process.exitCode = 130;
+		await stop();
+		process.exit();
+	});
+	process.once("SIGTERM", async () => {
+		process.exitCode = 143;
+		await stop();
+		process.exit();
+	});
 
-if (subcommand == null) {
-	throw new Error(`Unrecognized command: ${cmd}`);
-}
-
-let stopping: Promise<void> | false = false;
-function stop() {
-	if (!stopping)
-		stopping = (async () => {
-			try {
-				await subcommand?.cleanup?.();
-			} catch (e) {
-				console.error("Failed while running cleanup");
-				console.error(e);
-				process.exitCode = 1;
-			}
-		})();
-	return stopping;
-}
-process.once("SIGINT", async () => {
-	process.exitCode = 130;
-	await stop();
-	process.exit();
-});
-process.once("SIGTERM", async () => {
-	process.exitCode = 143;
-	await stop();
-	process.exit();
-});
-
-try {
-	await subcommand.run(rest);
-} catch (e) {
-	console.error(e);
-} finally {
-	await stop();
-}
+	try {
+		await subcommand.run(rest);
+	} catch (e) {
+		console.error(e);
+		await stop();
+	}
+})();
