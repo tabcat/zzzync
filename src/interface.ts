08b174c6a50052010f41a0f1ab1a64bb5cf9869e
tabcat <tabcat00@proton.me>
2026-01-23 16:01:15 +0530

    fix!: interface conforms to spec

--- src/interface.ts
+++ src/interface.ts
@@ -3,21 +3,18 @@ import type {
 	CID_VERSION_1,
 	CODEC_IDENTITY,
 	CODEC_LIBP2P_KEY,
-	CODEC_SHA2_256,
 } from "./constants.js";
 
 /**
- * CID<0x1, 0x72, 0x00 | 0x12>
+ * CID<0x1, 0x72, 0x00>
  */
 export type Libp2pKey = CID<
 	typeof CID_VERSION_1,
 	typeof CODEC_LIBP2P_KEY,
-	typeof CODEC_IDENTITY | typeof CODEC_SHA2_256
+	typeof CODEC_IDENTITY
 >;
 
 /**
- * MultihashDigest<0x00 | 0x12>
+ * MultihashDigest<0x00>
  */
-export type IpnsMultihash = MultihashDigest<
-	typeof CODEC_IDENTITY | typeof CODEC_SHA2_256
->;
+export type IpnsMultihash = MultihashDigest<typeof CODEC_IDENTITY>
