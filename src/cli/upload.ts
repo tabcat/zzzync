973ec9b8ccd7faee6bfd8da9fb73a62a178192e1
tabcat <tabcat00@proton.me>
2026-02-11 12:31:45 +0530

    fetch latest record from zzzync node

--- src/cli/upload.ts
+++ src/cli/upload.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
 import { car } from "@helia/car";
 import { globSource, unixfs } from "@helia/unixfs";
-import { privateKeyFromProtobuf } from "@libp2p/crypto/keys";
-import type { Libp2p } from "@libp2p/interface";
+import { generateKeyPair, privateKeyFromProtobuf } from "@libp2p/crypto/keys";
+import { Fetch } from "@libp2p/fetch";
+import type { Libp2p, PrivateKey } from "@libp2p/interface";
 import { enable, logger } from "@libp2p/logger";
-import { peerIdFromPrivateKey } from "@libp2p/peer-id";
 import { type Multiaddr, multiaddr } from "@multiformats/multiaddr";
 import { ipns } from "@tabcat/helia-ipns";
 import { LevelBlockstore } from "blockstore-level";
-import { MemoryDatastore } from "datastore-core";
+import { LevelDatastore } from "datastore-level";
 import { createHelia, type DefaultLibp2pServices } from "helia";
 import type { Libp2pOptions } from "libp2p";
 import { base36 } from "multiformats/bases/base36";
@@ -21,6 +21,7 @@ import { parseArgs } from "node:util";
 import { createSign, SupportedPrivateKey } from "../challenge.js";
 import { ZZZYNC_PROTOCOL_ID } from "../constants.js";
 import { UPLOAD_NAMESPACE, zzzync } from "../dialer.js";
+import { fetchIpnsRecord } from "../libp2p-fetch/ipns.js";
 import type { UploadConfig } from "./default-upload-config.js";
 import type { SubCommand } from "./index.js";
 import { command } from "./index.js";
@@ -37,14 +38,21 @@ declare global {
        * Comma separated multiaddr strings (<multiaddr>,<multiaddr>)
        */
       MULTIADDRS?: string;
+
+      /**
+       * Used to detect if running inside a Github Action
+       */
+      GITHUB_ACTIONS?: string;
     }
   }
 }
 
+const publisherKeyName = "publisher";
+const isGitHubAction = process.env.GITHUB_ACTIONS === "true";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// enable logging
 const log = logger(UPLOAD_NAMESPACE);
-
 let enabled = `${UPLOAD_NAMESPACE}, ${UPLOAD_NAMESPACE}:*`;
 if (process.env.DEBUG != null) {
   enabled = `${process.env.DEBUG},${enabled}`;
@@ -57,11 +65,15 @@ export let cleanup: SubCommand["cleanup"] = () => {};
 export const run: SubCommand["run"] = async (args: string[]) => {
   const { values, positionals } = parseArgs({
     args,
-    options: { config: { default: `./.${command}`, type: "string" } },
+    options: {
+      config: { default: `./.${command}`, type: "string" },
+      persisted: { default: false, type: "boolean" },
+    },
     strict: true,
     allowPositionals: true,
   });
 
+  // get upload path
   let upload: string | null = null;
   try {
     // just support single path uploads for now
@@ -69,62 +81,51 @@ export const run: SubCommand["run"] = async (args: string[]) => {
       upload = resolve(partial);
       break;
     }
+    if (upload == null) {
+      throw new Error("no files specified to upload.");
+    }
   } catch (e) {
     console.error("error while parsing file paths");
     throw e;
   }
 
-  if (upload == null) {
-    throw new Error("no files specified to upload.");
-  }
-
-  if (process.env.PUBLISHER_KEY == null) {
-    throw new Error(
-      "Did not find PUBLISHER_KEY in env. Use zzzync generate to create one. Then add to environment.",
+  if (values.persisted) {
+    console.log(
+      "persisted option passed - this assumes the latest ipns record is always local",
     );
-  }
-
-  let publisherKey: SupportedPrivateKey;
-  try {
-    const sk = privateKeyFromProtobuf(base36.decode(process.env.PUBLISHER_KEY));
-
-    if (sk.type !== "Ed25519" && sk.type !== "secp256k1") {
-      throw new Error("Unsupported key type");
+    if (isGitHubAction) {
+      throw new Error("persisted flag is not allowed in Github Actions");
     }
-
-    publisherKey = sk;
-  } catch (e) {
-    console.error(
-      "failed to parse PUBLISHER_KEY. Use zzzync generate to create one.",
-    );
-    throw e;
   }
 
+  // get multiaddrs
+  const multiaddrs: Multiaddr[] = [];
   if (process.env.MULTIADDRS == null) {
     throw new Error("no multiaddr provided");
   }
-
-  const multiaddrs: Multiaddr[] = [];
   try {
     for (const addr of process.env.MULTIADDRS.split(",")) {
-      multiaddrs.push(multiaddr(addr));
+      const ma = multiaddr(addr);
+      multiaddrs.push(ma);
+      log("found multiaddr %a", ma);
     }
   } catch (e) {
     console.error(
-      "failed to parse MULTIADDRS. Use zzzync daemon --multiaddrs to print them.",
+      "failed to parse MULTIADDRS. Use zzzync daemon to print them.",
     );
     throw e;
   }
 
+  // get config path
   if (
     !values.config.endsWith(`/.${command}`) && values.config !== `.${command}`
   ) {
-    throw new Error(`--dir directory must be named ".${command}"`);
+    throw new Error(`--config directory must be named ".${command}"`);
   }
   const CONFIG_DIR = resolve(values.config);
   await mkdir(CONFIG_DIR, { recursive: true });
 
-  const datastore = new MemoryDatastore();
+  const datastore = new LevelDatastore(join(CONFIG_DIR, "upload/datastore"));
   const blockstore = new LevelBlockstore(join(CONFIG_DIR, "upload/blockstore"));
 
   const DEFAULT_CONFIG_PATH = join(__dirname, "default-upload-config.js");
@@ -134,21 +135,15 @@ export const run: SubCommand["run"] = async (args: string[]) => {
     : DEFAULT_CONFIG_PATH;
   const config: UploadConfig = await import(CONFIG_PATH);
 
-  const peerId = peerIdFromPrivateKey(publisherKey);
+  const libp2pOptions: Libp2pOptions<
+    DefaultLibp2pServices & { fetch: Fetch; }
+  > = config.libp2pOptions != null
+    ? config.libp2pOptions
+    : (await import(DEFAULT_CONFIG_PATH)).libp2p;
 
-  log("peerId is %s", peerId);
-
-  const libp2p: Libp2pOptions<DefaultLibp2pServices> =
-    config.libp2pOptions != null
-      ? config.libp2pOptions
-      : (await import(DEFAULT_CONFIG_PATH)).libp2p;
-
-  const helia = await createHelia<Libp2p<DefaultLibp2pServices>>({
-    blockstore,
-    datastore,
-    libp2p: { ...libp2p, peerId },
-    start: false,
-  });
+  const helia = await createHelia<
+    Libp2p<DefaultLibp2pServices & { fetch: Fetch; }>
+  >({ blockstore, datastore, libp2p: libp2pOptions, start: false });
   const controller = new AbortController();
   const signal = controller.signal;
   cleanup = async () => {
@@ -157,8 +152,47 @@ export const run: SubCommand["run"] = async (args: string[]) => {
     await helia.stop();
     log("helia stopped.");
   };
-  await config?.beforeStart?.(helia);
 
+  const libp2p = helia.libp2p;
+
+  // get publisher key
+  let publisherKey: SupportedPrivateKey;
+  try {
+    let sk: PrivateKey;
+    if (values.persisted) {
+      log("persisted option passed - using datastore publisher key");
+      try {
+        sk = await libp2p.services.keychain.exportKey(publisherKeyName);
+      } catch (e) {
+        if (e instanceof Error && e.name === "NotFoundError") {
+          // If no named key found in keychain, generate and import
+          sk = await generateKeyPair("Ed25519");
+        } else {
+          throw e;
+        }
+      }
+    } else if (process.env.PUBLISHER_KEY) {
+      log("found PUBLISHER_KEY environment variable");
+
+      sk = privateKeyFromProtobuf(base36.decode(process.env.PUBLISHER_KEY));
+    } else {
+      throw new Error(
+        "Did not find PUBLISHER_KEY in env. Use zzzync generate to create one. Then add to environment.",
+      );
+    }
+
+    if (sk.type !== "Ed25519" && sk.type !== "secp256k1") {
+      throw new Error("Unsupported key type");
+    }
+
+    publisherKey = sk;
+  } catch (e) {
+    console.error(e);
+    throw new Error("unable to find publisher key");
+  }
+  await libp2p.services.keychain.importKey(publisherKeyName, publisherKey);
+
+  await config?.beforeStart?.(helia);
   log("starting helia...");
   await helia.start();
   log("helia started.");
@@ -169,13 +203,11 @@ export const run: SubCommand["run"] = async (args: string[]) => {
   const entry = await stat(upload);
 
   let root: CID | null = null;
+  log("importing %s to unixfs", upload);
   if (entry.isDirectory()) {
     log("path is to directory");
     for await (
-      const imported of importer.addAll(globSource(upload, "**/*"), {
-        signal,
-        wrapWithDirectory: true,
-      })
+      const imported of importer.addAll(globSource(upload, "**/*"), { signal })
     ) {
       log.trace(imported);
       root = imported.cid;
@@ -190,22 +222,46 @@ export const run: SubCommand["run"] = async (args: string[]) => {
   } else {
     throw new Error("upload path is not a file or directory.");
   }
+  log("imported %s into unixfs", upload);
 
   const names = ipns(helia);
   const exporter = car(helia);
 
-  await helia.libp2p.services.keychain.importKey("publisher", publisherKey);
+  log("dialing multiaddrs");
+  const connection = await libp2p.dial(multiaddrs, { signal });
+  const handlerPeerId = connection.remotePeer;
+  log("dial complete");
+
+  if (!values.persisted) {
+    log("fetching ipns record from zzzync handler");
+    const dialerIpns = publisherKey.publicKey.toMultihash();
+    const fetch = libp2p.services.fetch.fetch.bind(libp2p.services.fetch);
+    const handlerRecord = await fetchIpnsRecord(
+      fetch,
+      handlerPeerId,
+      dialerIpns,
+      { signal },
+    );
+    if (handlerRecord) {
+      log("importing record from handler");
+      await names.republish(dialerIpns, {
+        offline: true,
+        upkeep: "none",
+        record: handlerRecord,
+      });
+    } else {
+      log("fetch did not find record");
+    }
+  } else {
+    log("persisted option enabled - skipping fetch from zzzync handler");
+  }
+
   const published = await names.publish("publisher", root, {
     offline: true,
     signal,
     upkeep: "none",
   });
   log("created new ipns record");
-
-  log("dialing multiaddrs");
-  const connection = await helia.libp2p.dial(multiaddrs, { signal });
-  const handlerPeerId = connection.remotePeer;
-  log("dial complete");
 
   log("opening zzzync stream");
   const stream = await connection.newStream(ZZZYNC_PROTOCOL_ID, { signal });
