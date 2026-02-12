7346332007da094836bf903c3b8e3de241ee00e5
tabcat <tabcat00@proton.me>
2026-02-12 17:21:19 +0530

    zzzync generate improved messaging

--- src/cli/generate.ts
+++ src/cli/generate.ts
@@ -1,7 +1,24 @@
 import { generateKeyPair, privateKeyToProtobuf } from "@libp2p/crypto/keys";
 import { base36 } from "multiformats/bases/base36";
 import { parseArgs } from "node:util";
+import { SupportedPrivateKey } from "../challenge.js";
 import { SubCommand } from "./index.js";
+
+const contenthashMessage = (privateKey: SupportedPrivateKey) => `
+/ipns/${privateKey.publicKey.toCID().toString(base36)}
+
+This is the contenthash for the IPNS publishing key
+Use it to set the contenthash field for an ENS domain
+After content is uploaded, it can be resolved via ENS
+`;
+
+const publisherKeyMessage = (privateKey: SupportedPrivateKey) => `
+ZZZYNC_PUBLISHER_KEY=${base36.encode(privateKeyToProtobuf(privateKey))}
+
+This is the IPNS publishing key for the repo
+Add this to .env file in repo root
+Be sure to add .env to .gitignore
+`;
 
 export const run: SubCommand["run"] = async (args: string[]) => {
   const { values } = parseArgs({
@@ -16,12 +33,8 @@ export const run: SubCommand["run"] = async (args: string[]) => {
     );
   }
 
-  const privateKey = await generateKeyPair(values.type);
+  const privateKey = await generateKeyPair(values.type) as SupportedPrivateKey;
 
-  console.log(
-    `CONTENT_HASH=/ipns/${privateKey.publicKey.toCID().toString(base36)}`,
-  );
-  console.log(
-    `PUBLISHER_KEY=${base36.encode(privateKeyToProtobuf(privateKey))}`,
-  );
+  console.log(publisherKeyMessage(privateKey));
+  console.log(contenthashMessage(privateKey));
 };
